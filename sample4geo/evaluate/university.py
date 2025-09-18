0e44d8ea17bc1396bd15369d79f6afa29f4a0317
Indifferent <140402123+ISChenawei@users.noreply.github.com>
2025-09-18 16:45:43 +0800

    Add files via upload

--- sample4geo/evaluate/university.py
+++ sample4geo/evaluate/university.py
@@ -20,7 +20,8 @@ def evaluate(config,
     print("Extract Features:")
     img_features_gallery, ids_gallery,paths_gallery = predict(config, model, gallery_loader)
     img_features_query, ids_query,paths_query = predict(config, model, query_loader)
-
+    # print(img_features_query.shape)
+    # print(img_features_gallery.shape)
 
     gl = ids_gallery.cpu().numpy()
     ql = ids_query.cpu().numpy()
@@ -77,15 +78,22 @@ def eval_query(qf, ql, gf, gl,query_path,paths_gallery):
 
     # junk index
     junk_index = np.argwhere(gl == -1)
-    top_k_results = index[:top_k]
-    top_k_path =[paths_gallery[i] for i in top_k_results]
-  
+    # top_k_results = index[:top_k]
+    # top_k_path =[paths_gallery[i] for i in top_k_results]
+    # print(f"Query Image:{query_path}")
+    # save_folder='/home/hk/PAPER/DAC-main/vird'
+    #
+    # print(f"Query ID:{ql},Top {top_k} Gallery Results:{gl[top_k_results]}")
+    # for rank,gallery_path in enumerate(top_k_path,1):
+    #     plot_query_and_gallery(query_path, top_k_path, save_folder)
+    #     print(f"Top{rank} Gallery Image:{gallery_path}")
     ## T-tsne
     CMC_tmp = compute_mAP(index, good_index, junk_index)
     return CMC_tmp
+#
 def save_features_to_mat(query_features, gallery_features, query_labels, gallery_labels, paths_query, paths_gallery, save_path='features.mat'):
     """
-    Save the extracted features and labels to a .mat file
+    保存提取的特征和标签到 .mat 文件
     """
     result = {
         'query_features': query_features.cpu().numpy(),
@@ -97,6 +105,44 @@ def save_features_to_mat(query_features, gallery_features, query_labels, gallery
     }
     scipy.io.savemat(save_path, result)
     print(f'Features and labels saved to {save_path}')
+#
+# def plot_query_and_gallery(query_image_path,gallery_image_path,save_folder):
+#     fig,axes =plt.subplots(1,len(gallery_image_path) + 1,figsize = (20,5))
+#     if not os.path.exists(save_folder):
+#         os.makedirs(save_folder)
+#     query_folder = os.path.basename(os.path.dirname(query_image_path))
+#     split_path = query_image_path.split('query_satellite',1)[-1]
+#     query_filename = split_path.strip(os.sep)
+#
+#     output_filename = query_filename.replace(os.sep,"_")
+#     output_filename = f"{output_filename}_result.png"
+#     query_img = cv.imread(query_image_path)
+#     query_img = cv.cvtColor(query_img,cv.COLOR_BGR2RGB)
+#     axes[0].imshow(query_img)
+#     # axes[0].set.title('Query Image')
+#     axes[0].axis('off')
+#
+#     rect = patches.Rectangle((0,0),query_img.shape[1],query_img.shape[0],
+#                              linewidth=5 , edgecolor='orange',facecolor='none')
+#     axes[0].add_patch(rect)
+#
+#     for i,gallery_image_path in enumerate(gallery_image_path):
+#         gallery_img = cv.imread(gallery_image_path)
+#         gallery_img = cv.cvtColor(gallery_img,cv.COLOR_BGR2RGB)
+#         axes[i + 1].imshow(gallery_img)
+#         # axes[i + 1].set_totle(f'Gallery {i +1 }')
+#         axes[i + 1].axis('off')
+#         gallery_folder = os.path.basename(os.path.dirname(gallery_image_path))
+#         is_match = query_folder == gallery_folder
+#         rect_color ='green' if is_match else 'red'
+#         rect = patches.Rectangle((0, 0), query_img.shape[1], query_img.shape[0],
+#                                  linewidth=8, edgecolor=rect_color, facecolor='none')
+#         axes[i + 1].add_patch(rect)
+#     plt.tight_layout()
+#     out_path = os.path.join(save_folder,output_filename)
+#     plt.savefig(out_path)
+#     plt.close()
+#     print(f"Image saved to : {out_path}")
 def compute_mAP(index, good_index, junk_index):
     ap = 0
     cmc = torch.IntTensor(len(index)).zero_()
